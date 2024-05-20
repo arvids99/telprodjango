fcc3b9fa48fc44c70ead5caf753af70a9369442e
arvids <lielaispratnieks@inbox.lv>
2024-05-20 07:57:20 +0300

    example 2 - render HTML from template

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    html = """
-    <html>
-    <head>
-        <title>Hello World</title>
-        <style>
-            body { font-family: Arial, sans-serif; }
-            h1 { color: blue; }
-        </style>
-        <script>
-            function showAlert() {
-                alert('Hello from JavaScript!');
-            }
-        </script>
-    </head>
-    <body>
-        <h1>Hello, World!</h1>
-        <button onclick="showAlert()">Click me</button>
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'hello/hello_world.html')
