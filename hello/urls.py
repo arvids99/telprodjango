4350a40e42fadaa1da9b4a7d06ac007078a8c7d7
arvids <lielaispratnieks@inbox.lv>
2024-05-20 08:20:21 +0300

    implement message board

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, submit_message, get_messages
 
 urlpatterns = [
     path('', hello_world, name='hello_world'),
+    path('submit/', submit_message, name='submit_message'),
+    path('messages/', get_messages, name='get_messages'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MessageForm
+from .models import Message
+
 
 def hello_world(request):
     return render(request, 'hello/hello_world.html')
+
+def submit_message(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_messages')
+    else:
+        form = MessageForm()
+    return render(request, 'message_board/submit_message.html', {'form': form})
+
+def get_messages(request):
+    if request.method == 'GET':
+        name = request.GET.get('name')
+        sent_messages = Message.objects.filter(sender=name).order_by('-timestamp')[:20]
+        received_messages = Message.objects.filter(recipient=name).order_by('-timestamp')[:20]
+        return render(request, 'message_board/get_messages.html', {
+            'sent_messages': sent_messages,
+            'received_messages': received_messages,
+        })
